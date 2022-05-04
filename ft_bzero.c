ff43bdce38f67a92b01ebbf5c3d97374cd6d8b29
Yoonjyjy <xkwkdudhkd@naver.com>
2022-05-04 16:33:27 +0900

    일부수정

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: jiyoon <jiyoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 22:57:38 by jiyoon            #+#    #+#             */
-/*   Updated: 2022/03/16 01:51:23 by jiyoon           ###   ########.fr       */
+/*   Updated: 2022/05/04 16:14:53 by jiyoon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *ptr, size_t len)
+void	ft_bzero(void *s, size_t n)
 {
-	ft_memset(ptr, 0, len);
+	ft_memset(s, 0, n);
 }
